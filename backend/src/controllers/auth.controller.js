7a379985045af713cbdc22a1c122f8226e39299f
Jash Bohare <jashbohare@gmail.com>
2026-03-16 19:55:20 +0530

    Added login API

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -44,4 +44,46 @@ async function registerUser(req, res) {
   });
 }
 
-module.exports = { registerUser };
+async function loginUser(req, res) {
+  const { name, email, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [{ name }, { email }],
+  });
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  if (!password) {
+    return res.status(401).json({ message: "Password missing" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    res.status(401).json({ message: "Invaid credentials" });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      role: user.role,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User logged in successfully",
+    user: {
+      id: user._id,
+      username: user.name,
+      email: user.email,
+      role: user.role,
+    },
+  });
+}
+
+module.exports = { registerUser, loginUser };
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -5,5 +5,6 @@ const validationRules = require("../middlewares/validation.middleware")
 const router = express.Router();
 
 router.post("/register", validationRules.registerUserValidationRules, authController.registerUser);
+router.post("/login", authController.loginUser);
 
 module.exports = router;
